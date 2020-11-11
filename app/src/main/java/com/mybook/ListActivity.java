edc24fa7015992e4bc6db351890851f355c14424
kleiton.rodrigues <kleiton_rodrigues@hotmail.com>
2020-11-11 09:12:47 -0300

    Update File ListActivity

--- app/src/main/java/com/mybook/ListActivity.java
+++ app/src/main/java/com/mybook/ListActivity.java
@@ -1,24 +1,15 @@
 package com.mybook;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Base64;
-import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -31,7 +22,7 @@ import org.json.JSONObject;
 
 public class ListActivity extends AppCompatActivity {
     private RequestQueue mQueue2;
-    public String sValor="Kotlin";
+    public String sValueHtml;
     ProgressBar progressBar;
     private TextView tvReturn;
 
@@ -39,16 +30,17 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        // Inicializa referência dos componentes
+        tvReturn = findViewById(R.id.tvAlReturn);
+        mQueue2 = Volley.newRequestQueue(this);
+        // Armazena Consulta na Memoria do smartphone
+        SharedPreferences myPreferences = PreferenceManager.getDefaultSharedPreferences(ListActivity.this);
+        String history1 = myPreferences.getString("HISTORY1", "");
+        // Exibe configura ActionBar
         getSupportActionBar().show();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //
-        tvReturn = findViewById(R.id.tvAlReturn);
-        mQueue2 = Volley.newRequestQueue(this);
-        //
-        SharedPreferences myPreferences = PreferenceManager.getDefaultSharedPreferences(ListActivity.this);
-        String history1 = myPreferences.getString("HISTORY1", "");
         getSupportActionBar().setTitle("SEARCH");
         getSupportActionBar().setSubtitle("In Your Library | Search by: "+history1);
         getBooks(history1);
@@ -66,33 +58,52 @@ public class ListActivity extends AppCompatActivity {
                                 JSONObject book = jsonArray.getJSONObject(i);
                                 String artistIds = book.getString("artistIds");
                                 String artworkUrl60 = book.getString("artworkUrl60");
+                                String description = book.getString("description");
                                 String trackCensoredName = book.getString("trackCensoredName");
                                 String price = book.getString("price");
 
-                                tvReturn.append("<img src='"+artworkUrl60+"'><b>"+trackCensoredName + "</b> <br>R$ "+ price + "<br>" +
-                                        "<input type='button' value='Detail' onClick=\\\"alert('Hello Android!');\\\" />\n<hr>");
-                                sValor = tvReturn.getText().toString();
+                                tvReturn.append(
+                                  "<div class=\"col-sm\">\n" +
+                                        "   <img src='" + artworkUrl60 + "'>\n" +
+                                        "</div>" +
+                                        "<div class=\"col-sm\">\n" +
+                                        "   <h5>" + trackCensoredName + "</h5>\n" +
+                                        "   <p>R$ "+ price + "</p>\n" +
+                                        "   <p><a class=\"btn btn-info\" data-toggle=\"modal\" data-target=\"#modalExemplo"+artistIds+"\" role=\"button\">View details &raquo;</a></p>\n" +
+                                        "</div><div class=\"modal fade\" id=\"modalExemplo"+artistIds+"\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">\n" +
+                                          "  <div class=\"modal-dialog\" role=\"document\">\n" +
+                                          "    <div class=\"modal-content\">\n" +
+                                          "      <div class=\"modal-header\">\n" +
+                                          "        <h5>" + trackCensoredName + "</h5>\n" +
+                                          "        <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">\n" +
+                                          "          <span aria-hidden=\"true\">&times;</span>\n" +
+                                          "        </button>\n" +
+                                          "      </div>\n" +
+                                          "      <div class=\"modal-body\">\n" +
+                                          "      <p>R$ "+ description + "</p>\n" +
+                                          "      <p><strong>R$ "+ price + "</strong></p>\n" +
+                                          "      </div>\n" +
+                                          "      <div class=\"modal-footer\">\n" +
+                                          "        <button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">Close</button>\n" +
+                                          "      </div>\n" +
+                                          "    </div>\n" +
+                                          "  </div>\n" +
+                                          "</div>" +
+                                          "<hr>");
+                                sValueHtml = tvReturn.getText().toString();
                                 // Web View
                                 WebView mbrowser = (WebView) findViewById(R.id.webview_list);
                                 // Configurações da ProgressBar no WebView
                                 progressBar = (ProgressBar) findViewById(R.id.progressBar);
                                 progressBar.getIndeterminateDrawable().setColorFilter(0xFF77b3d4, android.graphics.PorterDuff.Mode.SRC_ATOP);
                                 progressBar.setMax(100);
-                                String unencodedHtml ="<!DOCTYPE html><html><head><script type=\"text/javascript\">\n" +
-                                        "    function showAndroidToast(toast) {\n" +
-                                        "        Android.showToast(toast);\n" +
-                                        "    }\n" +
-                                        "</script><body>"+
-                                        sValor+"</body></head></html>";
+                                String unencodedHtml ="<html><head><link href=\"https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\">" +
+                                        "</head><body><div class=\"container\"><div class=\"row\">" +
+                                        "          "+sValueHtml+"</div></div></body> <script src=\"https://code.jquery.com/jquery-3.2.1.slim.min.js\" integrity=\"sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN\" crossorigin=\"anonymous\"></script>" +
+                                        "    <script src=\"https://getbootstrap.com/docs/4.0/dist/js/bootstrap.min.js\"></script>\n</html>";
                                 String encodedHtml = Base64.encodeToString(unencodedHtml.getBytes(), Base64.NO_PADDING);
                                 mbrowser.loadData(encodedHtml, "text/html", "base64");
-                                //
                                 mbrowser.getSettings().setJavaScriptEnabled(true);
-                                /*class JsObject {
-                                    @JavascriptInterface
-                                    public String toString() { return "Android"; }
-                                }*/
-                                //mbrowser.addJavascriptInterface(new JsObject(), "Android");
                                 mbrowser.setWebViewClient(new ListActivity.HelloWebViewClient());
                                 mbrowser.setWebChromeClient(new WebChromeClient(){
                                     @Override
@@ -121,6 +132,7 @@ public class ListActivity extends AppCompatActivity {
         });
         mQueue2.add(request);
     }
+
     // Classe ultilizada para funcionalidade do ProgressBar
     private class HelloWebViewClient extends WebViewClient {
         @Override
@@ -137,26 +149,4 @@ public class ListActivity extends AppCompatActivity {
         return true;
     }
 
-    public void janela2(String title, String value){
-        AlertDialog alert = new AlertDialog.Builder(ListActivity.this).create();
-        alert.setTitle(title);
-        alert.setMessage(value);
-        alert.show();
-        //Toast.makeText(MainActivity.this, "Testesss", Toast.LENGTH_SHORT).show();
-    }
-
-    public class WebAppInterface extends Activity {
-        Context mContext;
-
-        /** Instantiate the interface and set the context */
-        WebAppInterface(Context c) {
-            mContext = c;
-        }
-
-        /** Show a toast from the web page */
-        @JavascriptInterface
-        public void showToast(String toast) {
-            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
-        }
-    }
 }
